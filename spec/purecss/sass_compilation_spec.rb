19e125d7e396b9518b65a23645afac994c90a06e
Dmitriy Tarasov <info@rubysamurai.com>
2015-07-22 15:59:19 +0300

    Configure tmp folder for rspec suite

--- spec/purecss/sass_compilation_spec.rb
+++ spec/purecss/sass_compilation_spec.rb
@@ -1,21 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe 'Sass compilation' do
-  before do
-    gem_path = File.expand_path('../..', File.dirname(__FILE__))
-    tmp_dir_path = File.join(gem_path, 'tmp')
-    FileUtils.mkdir_p(tmp_dir_path)
-  end
-
-  after do
-    FileUtils.rm_rf('.sass-cache', secure: true)
-    FileUtils.rm_rf('tmp', secure: true)
-  end
-
   it 'compiles scss to css' do
     path = 'vendor/assets/stylesheets'
     file = '_purecss'
-    sass_engine = Sass::Engine.for_file("#{path}/#{file}.scss", syntax: :scss, load_paths: [path])
+    sass_engine = Sass::Engine.for_file(
+                    "#{path}/#{file}.scss",
+                    cache_location: 'tmp/.sass-cache',
+                    load_paths:     [path],
+                    syntax:         :scss
+                  )
     expect {
       File.open("tmp/#{file}.css", 'w') { |f| f.write(sass_engine.render) }
     }.not_to raise_error
